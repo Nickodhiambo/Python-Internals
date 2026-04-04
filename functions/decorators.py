7a01274f3d42b204126725b2839e21d9873810d3
Nick <nodhiambo01@gmail.com>
2026-04-04 11:17:56 +0300

    Stack decorators

--- functions/decorators.py
+++ functions/decorators.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import time
 
 def logged(func):
     def wrapper(*args, **kwargs):
@@ -7,3 +8,23 @@ def logged(func):
         print(f'[LOG] {func.__name__} returns {result}')
         return result
     return wrapper
+
+
+def timed(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f'[TIMED] {func.__name__} ran in {end-start:.4f}s')
+        return result
+    return wrapper
+
+def validated(func):
+    def wrapper(name, *args, **kwargs):
+        if not isinstance(name, str) and not name.strip():
+            # Name should be a non empty string
+            raise ValueError('Variable name must be a non-empty string')
+        result = func(name, *args, **kwargs)
+        return result
+    return wrapper
+
