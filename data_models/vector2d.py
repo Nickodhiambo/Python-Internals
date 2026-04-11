a6ebb58c119ce8e7e173838088a5d3a91540b21a
Nick <nodhiambo01@gmail.com>
2026-04-11 12:56:41 +0300

    Modify vector2d

--- data_models/vector2d.py
+++ data_models/vector2d.py
@@ -24,8 +24,8 @@ import math
 
 class Vector:
     def __init__(self, x = 0, y = 0):
-        self.x = x
-        self.y = y
+        self.x = float(x)
+        self.y = float(y)
 
     def __add__(self, other):
         x = self.x + other.x
@@ -45,5 +45,15 @@ class Vector:
     def __bool__(self):
         return bool(abs(self))
 
+    def __iter__(self):
+        return (i for i in (self.x, self.y))
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
     def __repr__(self):
-        return f'Vector({self.x!r}, {self.y!r})'
+        class_name = type(self).__name__
+        return '{}({!r},{!r})'.format(class_name, *self)
+
+    def __str__(self):
+        return str(tuple(self))
