5ef6e7c61321403ad85454063129a16fc5986dd2
Nick <nodhiambo01@gmail.com>
2026-04-28 12:40:09 +0300

    Implement a multidimensional vector

---
+++ data_models/vector_nd.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+Implements a multidimensional vector that behaves like
+Python's native sequence type
+"""
+
+import math
+import reprlib
+from array import array
+from typing import Iterable
+
+class Vector:
+    typecode = 'd'
+    def __init__(self, components: Iterable):
+        self._components = array(self.typecode, components)
+
+    def __iter__(self):
+        return iter(self._components)
+
+    def __repr__(self):
+        components = reprlib.repr(self._components)
+        components = components[components.find('[')\
+                :-1]
+        return f'Vector({components})'
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
+    def abs(self):
+        return math.hypot(*self)
+
+    def __bool__(self):
+        return bool(abs(self))
